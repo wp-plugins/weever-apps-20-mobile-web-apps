ff81fc0aa9d38c69b520a5d47b7fbb29ca3c1bf2
brianhogg <brianhogg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-14 16:17:47 +0000

    tagging version 2.1.3
    git-svn-id: https://plugins.svn.wordpress.org/weever-apps-20-mobile-web-apps/tags/2.1.3@681858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-weever-const.php
+++ classes/class-weever-const.php
@@ -2,7 +2,7 @@
 
 final class WeeverConst
 {
-    const VERSION		= '2.1';
+    const VERSION		= '2.1.3';
     const RELEASE_TYPE	= '';
     const RELEASE_NAME	= '2.0 BETA';
     const NAME			= 'Weever Apps - appBuilder(tm) for Wordpress';
